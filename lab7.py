71d4adedefe29de6243b4f186d0563c5838c9053
NathanSeitz <71901625+NathanSeitz@users.noreply.github.com>
2020-11-11 17:25:33 -0700

    Update lab7.py

--- lab7.py
+++ lab7.py
@@ -1,14 +1,17 @@
 lab 7
 
 def new_contact_store():
-...
+def add_new_contact(contacts, first_name, last_name, email, phone_number, birthday):
+def has_contact(contacts, first_name, last_name):
+def get_contact_string(contacts, first_name, last_name):
+def has_contact(contacts, first_name, last_name):
+def remove_contact(contacts, first_name, last_name):
+def has_contact(contacts, first_name, last_name):
+def get_contact_string(contacts, first_name, last_name):
+def update_contact_first_name(contacts, first_name, last_name, new_field_value):
+def update_contact_last_name(contacts, first_name, last_name, new_field_value):
+def update_contact_email(contacts, first_name, last_name, new_field_value):
+def update_contact_phone_number(contacts, first_name, last_name, new_field_value):
+def update_contact_birthday(contacts, first_name, last_name, new_field_value):
 
-def add_new_contact():
-..
-
-[
- ['John', 'Smith','jsmith@gmail.com', '928-111-111', 'Nov. 10th'],
-
-
-def some_method_nam():
-    
+# Contact layout as follows: ['John', 'Smith','jsmith@gmail.com', '928-111-111', 'Nov. 10th']
